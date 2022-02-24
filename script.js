95b447acd197d8950704bac654ce562044925595
David Ernst <git@dsernst.com>
2022-02-24 14:18:47 -0800

    Add options & implement ObfuscateEmails func

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 const $input = document.getElementById('input')
 const $output = document.getElementById('output')
+const $reverse = document.getElementById('reverse')
+const $obfuscateEmails = document.getElementById('obfuscateEmails')
+const $indentBodies = document.getElementById('indentBodies')
 
 $input.oninput = () => {
   const input = $input.value
 
-  $output.innerHTML = input
+  const emailRegEx = /<(\w+)@(\w+)\.(\w+)> wrote:/g
+
+  let cleaned = input
+
+  if ($obfuscateEmails.checked) {
+    cleaned = input.replace(emailRegEx, (...args) => {
+      const [, name, domain, tld] = args.map((x) =>
+        typeof x !== 'string' ? x : x.slice(0, 1) + '...'
+      )
+      return `<${name}@${domain}.${tld}> wrote:`
+    })
+  }
+
+  $output.innerHTML = cleaned
 }
